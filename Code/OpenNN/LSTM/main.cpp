b5eaf98e148063e091095e1f8eaeb3da7bd3d157
Aurelio <aurelio.marin@mail.polimi.it>
2020-06-11 18:11:51 +0200

    Delete unneccesary

--- Code/OpenNN/LSTM/main.cpp
+++ Code/OpenNN/LSTM/main.cpp
@@ -9,14 +9,12 @@ using namespace OpenNN;
 
 int main()
 {
-         srand(static_cast<unsigned>(time(nullptr)));
+    srand(static_cast<unsigned>(time(nullptr)));
 
-        // Data set Reading
-
-        //DataSet data_set("../Data/shampo_sales.csv", ',', true);
+    // Data set Reading
 
     DataSet data_set;
-        data_set.set_data_file_name("../../Data/shampo_sales.csv");
+    data_set.set_data_file_name("../../Data/shampo_sales.csv");
     data_set.set_separator(',');
     data_set.set_has_columns_names(true);
     size_t lags_number = 4;
@@ -40,60 +38,60 @@ int main()
     data_set.split_instances_sequential();
 
 
-        // Neural network
+    // Neural network
 
-        NeuralNetwork neural_network;
+    NeuralNetwork neural_network;
 
-        // Scaling layer
+    // Scaling layer
 
-        ScalingLayer* scaling_layer = new ScalingLayer(inputs_number);
-        scaling_layer->set_scaling_methods(ScalingLayer::NoScaling);
+    ScalingLayer* scaling_layer_prt = new ScalingLayer(inputs_number);
+    scaling_layer_prt->set_scaling_methods(ScalingLayer::NoScaling);
 
-        neural_network.add_layer(scaling_layer);
+    neural_network.add_layer(scaling_layer_prt);
 
-        const size_t scaling_layer_outputs_dimensions = scaling_layer->get_neurons_number();
+    const size_t scaling_layer_outputs_dimensions = scaling_layer_ptr->get_neurons_number();
 
 
-        LongShortTermMemoryLayer* long_short_term_memory_layer_1 = new LongShortTermMemoryLayer(scaling_layer_outputs_dimensions, 4);
+    LongShortTermMemoryLayer* long_short_term_memory_layer_1_ptr = new LongShortTermMemoryLayer(scaling_layer_outputs_dimensions, 4);
 
-        neural_network.add_layer(long_short_term_memory_layer_1);
+    neural_network.add_layer(long_short_term_memory_layer_1_ptr);
 
-        const size_t lstm_layer_1_outputs = long_short_term_memory_layer_1->get_neurons_number();
+    const size_t lstm_layer_1_outputs = long_short_term_memory_layer_1_ptr->get_neurons_number();
 
-        PerceptronLayer* perceptron_layer_1 = new PerceptronLayer(lstm_layer_1_outputs, 1);
+    PerceptronLayer* perceptron_layer_1_ptr = new PerceptronLayer(lstm_layer_1_outputs, 1);
 
-        neural_network.add_layer(perceptron_layer_1);
+    neural_network.add_layer(perceptron_layer_1_ptr);
 
-        neural_network.print_summary();
+    neural_network.print_summary();
 
-        // Training strategy
-        TrainingStrategy training_strategy(&neural_network, &data_set);
-        training_strategy.set_optimization_method(TrainingStrategy::OptimizationMethod::ADAPTIVE_MOMENT_ESTIMATION);
-        training_strategy.set_loss_method(TrainingStrategy::LossMethod::MEAN_SQUARED_ERROR);
-        training_strategy.get_loss_index_pointer()->set_regularization_method(LossIndex::RegularizationMethod::NoRegularization);
+    // Training strategy
+    TrainingStrategy training_strategy(&neural_network, &data_set);
+    training_strategy.set_optimization_method(TrainingStrategy::OptimizationMethod::ADAPTIVE_MOMENT_ESTIMATION);
+    training_strategy.set_loss_method(TrainingStrategy::LossMethod::MEAN_SQUARED_ERROR);
+    training_strategy.get_loss_index_pointer()->set_regularization_method(LossIndex::RegularizationMethod::NoRegularization);
 
-        AdaptiveMomentEstimation * 	 adam_pointer = training_strategy.get_adaptive_moment_estimation_pointer() ;
+    AdaptiveMomentEstimation * 	 adam_ptr = training_strategy.get_adaptive_moment_estimation_pointer() ;
 
-        adam_pointer->set_maximum_epochs_number(3000);
-        adam_pointer->set_display_period(100);
-        adam_pointer->set_maximum_time(1800);
+    adam_ptr->set_maximum_epochs_number(3000);
+    adam_ptr->set_display_period(100);
+    adam_ptr->set_maximum_time(1800);
 
-        const OptimizationAlgorithm::Results training_strategy_results = training_strategy.perform_training();
-        // Testing analysis
+    const OptimizationAlgorithm::Results training_strategy_results = training_strategy.perform_training();
+    // Testing analysis
 
-        TestingAnalysis testing_analysis(&neural_network, &data_set);
-        cout<<endl<<"Testing Analysis"<<endl;
-        Vector< double > TestError=testing_analysis.calculate_testing_errors();
-        cout<<"Sum Squared error  :"<<TestError[0]<<endl;
-        cout<<"Mean Squared error   :"<<TestError[1]<<endl;
-        cout<<"Root Mean Squared error   :"<<TestError[2]<<endl;
-        cout<<"Normalized Squared error   :"<<TestError[3]<<endl;
+    TestingAnalysis testing_analysis(&neural_network, &data_set);
+    cout<<endl<<"Testing Analysis"<<endl;
+    Vector< double > TestError=testing_analysis.calculate_testing_errors();
+    cout<<"Sum Squared error  :"<<TestError[0]<<endl;
+    cout<<"Mean Squared error   :"<<TestError[1]<<endl;
+    cout<<"Root Mean Squared error   :"<<TestError[2]<<endl;
+    cout<<"Normalized Squared error   :"<<TestError[3]<<endl;
 
-        const TestingAnalysis::LinearRegressionAnalysis linear_regression_results = testing_analysis.perform_linear_regression_analysis()[0];
-        cout << "Linear Regression analysis"<<endl;
-        cout<<"Correlation    : " << linear_regression_results.correlation << endl;
+    const TestingAnalysis::LinearRegressionAnalysis linear_regression_results = testing_analysis.perform_linear_regression_analysis()[0];
+    cout << "Linear Regression analysis"<<endl;
+    cout<<"Correlation    : " << linear_regression_results.correlation << endl;
 
-        // Save results
+    // Save results
 
     data_set.save("data_set.xml");
 
@@ -104,6 +102,6 @@ int main()
     linear_regression_results.save("linear_regression_analysis_results.dat");
     TestError.save("Error.dat");
 
-        return 0;
+    return 0;
 
 }
